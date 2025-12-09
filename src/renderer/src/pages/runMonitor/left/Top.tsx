8c8785d726103d0e6d60f18ecb9496d308b09433
chenpeng <chenpeng1@persagy.com>
2025-12-09 22:41:40 +0800

    feat(styles): 增加字体粗细和字体大小的CSS辅助类，优化样式和组件布局
    - 在CSS样式中新增字体粗细相关类（f-w-300至f-w-700）
    - 添加字体大小相关类（text-xs至text-5xl）
    - 更新Top组件，优化卫生间功能类型的展示逻辑
    - 调整样式以改善组件的视觉效果和布局

--- src/renderer/src/pages/runMonitor/left/Top.tsx
+++ src/renderer/src/pages/runMonitor/left/Top.tsx
@@ -3,32 +3,34 @@ import { FC, useContext, useEffect, useState } from 'react'
 import styles from './styles/top.module.less'
 import { AppContext } from '../contexts'
 import { ToiletSpaceItem } from '../types'
-import { uniqBy } from 'lodash'
 
 interface TopProps {}
 interface ToiletRoomFuncTypes extends Partial<ToiletSpaceItem> {
   total: number
 }
-const Top: FC<TopProps> = props => {
+const Top: FC<TopProps> = () => {
   const { toiletSpaceMap } = useContext(AppContext)
   const [toiletRoomFuncTypes, setToiletRoomFuncTypes] = useState<ToiletRoomFuncTypes[]>([])
   useEffect(() => {
     if (toiletSpaceMap) {
-      const arr = [
-        {
-          roomFuncType: 'all',
-          roomFuncTypeName: '卫生间总数',
-          total: toiletSpaceMap.size
-        }
-      ]
-      Array.from(toiletSpaceMap.values()).forEach(item => {
-        arr.push({
-          roomFuncType: item.roomFuncType,
-          roomFuncTypeName: item.roomFuncTypeName,
-          total: arr.filter(i => i.roomFuncType === item.roomFuncType).length
-        })
+      const map = new Map()
+      map.set('all', {
+        roomFuncType: 'all',
+        roomFuncTypeName: '卫生间总数',
+        total: toiletSpaceMap.size
       })
-      setToiletRoomFuncTypes(uniqBy(arr, 'roomFuncType'))
+      Array.from(toiletSpaceMap.values()).forEach(item => {
+        if (map.has(item.roomFuncType)) {
+          map.get(item.roomFuncType).total += 1
+        } else {
+          map.set(item.roomFuncType, {
+            roomFuncType: item.roomFuncType,
+            roomFuncTypeName: item.roomFuncTypeName,
+            total: 1
+          })
+        }
+      })
+      setToiletRoomFuncTypes(Array.from(map.values()))
     }
   }, [toiletSpaceMap])
   return (
@@ -36,8 +38,12 @@ const Top: FC<TopProps> = props => {
       <h2 className={cn('f-w-700')}>智慧卫生间</h2>
       <div className={cn(styles.top_content)}>
         {toiletRoomFuncTypes.map(item => (
-          <div className={cn(styles.top_content_item)} key={item.roomFuncType}>
-            {item.roomFuncTypeName}
+          <div className={cn(styles.top_content_item, 'fc')} key={item.roomFuncType}>
+            <div className={cn(styles.item_chart)}></div>
+            <div className={cn(styles.item_name)}>
+              <div className={cn('text-24', 'f-w-700')}>{item.total}</div>
+              <div style={{ color: 'var(--color-text-tertiary)' }}>{item.roomFuncTypeName}</div>
+            </div>
           </div>
         ))}
       </div>
